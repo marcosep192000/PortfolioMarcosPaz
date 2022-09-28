6cf924f8c7f907ceb1c5ad99015dec29b7a8a600
marcos <marcosep192000@hotmail.com>
2022-09-28 03:27:02 -0300

    solo carpeta controller

---
+++ src/main/java/com/portfolio/PortfolioMarcosPaz/controller/SkillController.java
@@ -0,0 +1,11 @@
+package com.portfolio.PortfolioMarcosPaz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/skills")
+public class SkillController {
+
+}
