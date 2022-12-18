6fd44c099ce5ed5a97f707438ac620750568e820
marcos <marcosep192000@hotmail.com>
2022-12-18 10:33:53 -0300

    PROBANDO BASE DE DATOS

--- src/main/java/com/portfolio/PortfolioMarcosPaz/RunnerRoles.java
+++ src/main/java/com/portfolio/PortfolioMarcosPaz/RunnerRoles.java
@@ -27,7 +27,7 @@ UsuarioService usuario;
 
     @Override
     public void run(String... args) throws Exception {
-      /* System.out.println("Insert MySQL");
+   /*  System.out.println("Insert MySQL");
             Rol r = new Rol(ROLE_ADMIN);
             Rol S = new Rol(ROLE_ADMIN);
         rolService.save(r);
